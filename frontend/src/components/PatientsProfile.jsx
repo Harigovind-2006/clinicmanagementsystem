855ac2311596e19ffea4bafc2bfb741b63d554f7
Daniel <danieljoshyvymelil@gmail.com>
2026-06-17 16:47:47 +0530

    patient profile

--- frontend/src/components/PatientsProfile.jsx
+++ frontend/src/components/PatientsProfile.jsx
@@ -16,15 +16,19 @@ export default function PatientDetails() {
   const status = {
     type: "OP",
     lastBill: "2026-05-20",
+    admitted: "2026-06-01",
+    paymentUpto : "2026-06-10"
   };
 
-  const appointment = {
-    token: "#1",
+  const DoctorNotes = {
     doctor: "Dr. Amit Sharma",
-    time: "09:00 AM",
-    date: "2026-06-17",
-    status: "Waiting",
+    DoctorNotes: "Patient recovering well. Continue current medication.",
+    bp: "Blood Pressure: 130/85",
   };
+    const bill = {
+    
+
+    };
 
   return (
     <div className="p-6 ml-8">
@@ -114,14 +118,27 @@ export default function PatientDetails() {
             </h2>
 
             <div className="flex justify-between mb-4">
-              <span className="text-gray-500">Type</span>
-
-              <span className="bg-green-100 t   ext-green-700 px-3 py-1 rounded-md text-sm font-medium">
-                {status.type}
-              </span>
+                 <span className="text-gray-500"> Type</span>
+                <span
+                    className={`px-3 py-1 rounded-md text-sm font-medium ${
+                      status.type === "OP"
+                        ? "bg-blue-100 text-blue-700"
+                         : "bg-green-100 text-green-700"
+                    }`}
+                 >
+                  {status.type}
+                 </span>
+            </div>
+             <div className="flex justify-between mb-4">
+              <span className="text-gray-500"> Admitted</span>
+              <span>{status.admitted}</span>
+            </div>
+            <div className="flex justify-between  mb-4">
+              <span className="text-gray-500"> Payment Up To</span>
+              <span>{status.paymentUpto}</span>
             </div>
 
-            <div className="flex justify-between">
+            <div className="flex justify-between  mb-4">
               <span className="text-gray-500">Last Bill</span>
               <span>{status.lastBill}</span>
             </div>
@@ -130,37 +147,26 @@ export default function PatientDetails() {
          
           <div className="bg-white border border-gray-300 rounded-2xl p-6">
             <h2 className="text-2xl font-semibold mb-6">
-              Current Appointment
+             Doctor Notes
             </h2>
 
             <div className="space-y-4">
               <div className="flex justify-between">
-                <span className="text-gray-500">Token</span>
-                <span>{appointment.token}</span>
+                <span className="text-gray-500">{DoctorNotes.token}</span>
               </div>
 
               <div className="flex justify-between">
-                <span className="text-gray-500">Doctor</span>
-                <span>{appointment.doctor}</span>
+                <span className="text-gray-500">{DoctorNotes.doctor}</span>
               </div>
 
               <div className="flex justify-between">
-                <span className="text-gray-500">Time</span>
-                <span>{appointment.time}</span>
+                <span className="text-gray-500">{DoctorNotes.DoctorNotes}</span>
               </div>
 
               <div className="flex justify-between">
-                <span className="text-gray-500">Date</span>
-                <span>{appointment.date}</span>
+                <span className="text-gray-500">{DoctorNotes.bp}</span>
               </div>
-
-              <div className="flex justify-between">
-                <span className="text-gray-500">Status</span>
-
-                <span className="bg-blue-100 text-blue-700 px-3 py-1 rounded-md text-sm font-medium">
-                  {appointment.status}
-                </span>
-              </div>
+            
             </div>
           </div>
         </div>
