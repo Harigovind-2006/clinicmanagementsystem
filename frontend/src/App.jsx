69250c6a49fbff28832142c712e436daf264eb14
Harigovind M <harigovind.m@zeronetraining.local>
2026-06-19 11:26:49 +0530

    history created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SeniorDashboard from "./pages/SeniorDoctor/SeniorDashboard";
 import BillPayment from "./components/BillComponent/BillPayment";
 import BillDashboard from "./components/BillComponent/BillDashboard";
 import Admission from "./components/AdmissionComponent/Admission"
+
 function App() {
   return (
     <Routes>
@@ -18,6 +19,7 @@ function App() {
       <Route path="/patients/:pid" element={<SeniorDashboard />} />
       <Route path="/billing/:pid" element={<BillDashboard />} />
       <Route path="/admission" element={<Admission />} />
+      
     </Routes>
   );
 }
