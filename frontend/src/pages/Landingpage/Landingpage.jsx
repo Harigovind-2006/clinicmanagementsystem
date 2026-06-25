f17c89e84879f6eb6dd7fa85d8885ba142f790b6
Daniel <danieljoshyvymelil@gmail.com>
2026-06-25 10:30:55 +0530

    added responsiveness

--- frontend/src/pages/Landingpage/Landingpage.jsx
+++ frontend/src/pages/Landingpage/Landingpage.jsx
@@ -103,8 +103,8 @@ export default function Landingpage() {
   };
 
   return (
-    <section className="h-screen w-full flex items-center justify-end pr-8 lg:pr-24 bg-[url('/loginbg.png')] bg-cover bg-center bg-no-repeat">
-      <div className="w-full max-w-[500px] bg-white/95 backdrop-blur-sm rounded-3xl shadow-2xl p-10 mx-4">
+   <section className="min-h-screen w-full flex items-center justify-center lg:justify-end bg-[url('/loginbg.png')] bg-cover bg-center bg-no-repeat px-4 sm:px-6 lg:px-20 py-6">
+      <div className="w-full max-w-md sm:max-w-lg bg-white/95 backdrop-blur-sm rounded-3xl shadow-2xl p-6 sm:p-8 lg:p-10 mx-4">
 
         {/* Logo */}
         <div className="flex justify-center mb-6">
@@ -184,30 +184,30 @@ export default function Landingpage() {
           </div>
 
           {/* Password */}
-          <div>
-  <label className="block mb-2 font-semibold text-slate-700">
-    Password
-  </label>
+   <div>
+      <label className="block mb-2 font-semibold text-slate-700">
+        Password
+      </label>
 
-  <div className="relative">
-      <input
-        type={showPassword ? "text" : "password"}
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        placeholder="Enter password"
-        className="w-full border border-gray-300 rounded-xl px-4 py-3 pr-12 focus:border-blue-500 focus:outline-none"
-        required
-      />
+      <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Enter password"
+            className="w-full border border-gray-300 rounded-xl px-4 py-3 pr-12 focus:border-blue-500 focus:outline-none"
+            required
+          />
 
-      <button
-        type="button"
-        onClick={() => setShowPassword(!showPassword)}
-        className="absolute right-4 top-1/2 -translate-y-1/2 -translate-y-1/2 text-gray-500 "
-      >
-        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-      </button>
-    </div>
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-4 top-1/2 -translate-y-1/2 -translate-y-1/2 text-gray-500 "
+          >
+            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+          </button>
+      </div>
   </div>
 
           {/* Login Button */}
