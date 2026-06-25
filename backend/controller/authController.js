7b5e43c085618a8f9b2c96f960b9d75b5e7745bf
Harigovind M <harigovind.m@zeronetraining.local>
2026-06-25 11:07:27 +0530

    my commit

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 
@@ -15,8 +15,11 @@ export const loginUser = async (req, res) => {
         if (!user) {
             return res.status(401).json({ success: false, message: "Invalid username or password" });
         }
+        
+
 
         const isMatch = await bcrypt.compare(password, user.password);
+        console.log("Password match:", isMatch); // Debugging line
         if (!isMatch) {
             return res.status(401).json({ success: false, message: "Invalid username or password" });
         }
